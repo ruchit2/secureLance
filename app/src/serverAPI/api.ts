6a3cc0b3f803d09c47acc0294b36a7f8243a63e7
Ruchit Bhandari <ruchitbhandari1@gmail.com>
2024-04-05 16:16:28 +0530

    api param name update

--- app/src/serverAPI/api.ts
+++ app/src/serverAPI/api.ts
@@ -2,7 +2,7 @@ import server from "./server";
 
 const serverAPI = {
   async addNewContractAddress(address) {
-    return await server.post("new-contract", { contract: address });
+    return await server.post("new-contract", { address });
   },
   async getContractAddresses() {
     return await server.get("contracts");
