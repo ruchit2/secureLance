b195e4a25cfe5a914fc0822cd83b4f926ac40466
Ruchit Bhandari <ruchitbhandari1@gmail.com>
2024-04-05 16:05:25 +0530

    using db instead of file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
-import fs from "fs";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const port = 3042;
 const app = express();
@@ -8,30 +11,34 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-function readDataFromFile() {
-  const data = fs.readFileSync("db.json", "utf8");
-  const jsonData = JSON.parse(data);
-  return jsonData.contracts;
-}
+mongoose.connect(process.env.MONGODB_URL, {dbName: "db"});
 
-async function writeDataToFile(data) {
-  const jsonData = JSON.stringify(data, null, 2); // Convert data to JSON format
-  fs.writeFileSync("db.json", jsonData, "utf8");
-}
-
-app.get("/contracts",  (req, res) => {
-  let contracts = readDataFromFile();
-  res.send({ contracts });
+const contractSchema = new mongoose.Schema({
+  address: String,
 });
 
-app.post("/new-contract",  (req, res) => {
-  const { contract } = req.body;
-  if (!contract)
-    return res.status(400).send({ message: "Not specified contract address!" });
-  let contracts = readDataFromFile();
-  contracts.push(contract);
-  writeDataToFile({ contracts });
-  return res.send({ amount: contracts.length });
+const Contract = mongoose.model("Contract", contractSchema);
+
+app.get("/contracts", async (req, res) => {
+  try {
+    const contracts = await Contract.find();
+    res.send({ contracts: contracts.map((contractObj) => contractObj.address) });
+  } catch (error) {
+    res.status(500).send({ message: "Error fetching contracts" });
+  }
+});
+
+app.post("/new-contract", async (req, res) => {
+  const { address } = req.body;
+  if (!address)
+    return res.status(400).send({ message: "Contract address not specified!" });
+  try {
+    const newContract = new Contract({ address });
+    await newContract.save();
+    res.send({ message: "Contract added successfully" });
+  } catch (error) {
+    res.status(500).send({ message: "Error adding contract" });
+  }
 });
 
 app.listen(port, () => {
